87919c34bbd72d9a98eaa54518cbd4b5da07d135
rushi444 <rushi44@gmail.com>
2019-11-01 20:05:07 -0700

    Auth middleware, protected login route

--- Back-end/routes/auth.js
+++ Back-end/routes/auth.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 router.post(
